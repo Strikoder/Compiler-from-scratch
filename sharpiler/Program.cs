d00d6b1cdf954cdc9b7914394421c7f7edde6978
strikoder <ameen.yaseen.98@gmail.com>
2024-01-28 18:24:54 +0300

    Add initial files

---
+++ sharpiler/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace sharpiler
+{
+   class Program
+   {
+	   static void Main(string [] args)
+	   {
+		   while(true)
+		   {
+			   Console.Write("> ");
+			   var line = Console.ReadLine();
+			   if (string.IsNullOrWhiteSpace(line))
+				   return;
+			   if (line == "1+2+3")
+				   Console.WriteLine("7");
+			   else
+			       Console.WriteLine("Error: Invalid Expression");
+		   }
+	   }
+
+	   enum SynatxKind
+	   {
+
+	   }
+
+	   class SyntaxToken
+	   {
+		   public SyntaxToken(SyntaxKind kind, int position, string text)
+		   {
+			   Kind=kind;
+			   Position=position;
+			   Text=text;
+		   }
+
+		   public SyntaxKind Kind {get;}
+		   public int Position {get;}
+		   public string Text {get;}
+	   }
+
+	   class Lexer
+	   {
+		   private readonly string _text;
+		   private int _position;
+
+		   public Lexer(string text)
+		   {
+			   _text=text;
+		   }
+
+		   public SyntaxToken NextToken()
+		   {
+			   //numbers
+
+		   }
+
+		   
+	   }
+       
+   } 
+}
